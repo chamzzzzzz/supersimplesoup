d9ed0f74b3b2ea3a9d99ed92fe289914910c8351
Liu Chen <chamzzzzzz@gmail.com>
2022-12-30 23:26:17 +0800

    add test and comment

--- node.go
+++ node.go
@@ -1,3 +1,6 @@
+/*
+Package supersimplesoup implements a super simple soup like DOM API.
+*/
 package supersimplesoup
 
 import (
@@ -19,6 +22,9 @@ var (
 
 type Node html.Node
 
+// Parse returns the parse tree for the HTML from the given Reader.
+//
+// The input is assumed to be UTF-8 encoded.
 func Parse(r io.Reader) (*Node, error) {
 	if n, err := html.Parse(r); err != nil {
 		return nil, err
@@ -27,8 +33,17 @@ func Parse(r io.Reader) (*Node, error) {
 	}
 }
 
+// WalkFunc is the type of the function called by Walk to visit each node.
+//
+// The error result returned by the function controls how Walk continues.
+// 	- If the function returns the special value SkipNode, Walk skips the current node.
+// 	- If the function returns the special value SkipAll, Walk stops entirely and returns nil.
+//	- If the function returns a non-nil error, Walk stops entirely and returns that error.
 type WalkFunc func(node *Node) error
 
+// Walk walks the node tree rooted at root, calling fn for each node in the tree, including root.
+//
+// The nodes are walked in depth first order, which makes the output deterministic.
 func Walk(root *Node, fn WalkFunc) error {
 	err := walk(root, fn)
 	if err == SkipNode || err == SkipAll {
@@ -55,26 +70,32 @@ func walk(node *Node, fn WalkFunc) error {
 	return nil
 }
 
+// ParentNode returns the parent node of this node.
 func (n *Node) ParentNode() *Node {
 	return (*Node)(n.Parent)
 }
 
+// FirstChildNode returns the first direct child node of this node.
 func (n *Node) FirstChildNode() *Node {
 	return (*Node)(n.FirstChild)
 }
 
+// LastChildNode returns the last direct child node of this node.
 func (n *Node) LastChildNode() *Node {
 	return (*Node)(n.LastChild)
 }
 
+// PrevSiblingNode returns the previous sibling node of this node.
 func (n *Node) PrevSiblingNode() *Node {
 	return (*Node)(n.PrevSibling)
 }
 
+// NextSiblingNode returns the next sibling node of this node.
 func (n *Node) NextSiblingNode() *Node {
 	return (*Node)(n.NextSibling)
 }
 
+// Children returns all the direct child nodes of this node.
 func (n *Node) ChildrenNodes() (children []*Node) {
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
 		children = append(children, (*Node)(c))
@@ -82,8 +103,19 @@ func (n *Node) ChildrenNodes() (children []*Node) {
 	return
 }
 
+// IsElementNode returns whether is an element node.
+func (n *Node) IsElementNode() bool {
+	return n.Type == html.ElementNode
+}
+
+// IsTextNode returns whether is a text node.
+func (n *Node) IsTextNode() bool {
+	return n.Type == html.TextNode
+}
+
+// Attributes returns all the attributes key-value map of this node.
 func (n *Node) Attributes() map[string]string {
-	if n.Type != html.ElementNode || len(n.Attr) == 0 {
+	if !n.IsElementNode() || len(n.Attr) == 0 {
 		return nil
 	}
 	attrs := make(map[string]string, len(n.Attr))
@@ -93,6 +125,7 @@ func (n *Node) Attributes() map[string]string {
 	return attrs
 }
 
+// Attribute return the key specified attribute of this node.
 func (n *Node) Attribute(key string) string {
 	for _, attr := range n.Attr {
 		if attr.Key == key {
@@ -102,31 +135,37 @@ func (n *Node) Attribute(key string) string {
 	return ""
 }
 
+// ID returns the id attribute of this node.
 func (n *Node) ID() string {
 	return n.Attribute("id")
 }
 
+// Class returns the class attribute of this node.
 func (n *Node) Class() string {
 	return n.Attribute("class")
 }
 
+// Href returns the href attribute of this node.
 func (n *Node) Href() string {
 	return n.Attribute("href")
 }
 
+// Title returns the title attribute of this node.
 func (n *Node) Title() string {
 	return n.Attribute("title")
 }
 
+// HTML returns the HTML source code of this node.
 func (n *Node) HTML() string {
 	var buf bytes.Buffer
 	html.Render(&buf, (*html.Node)(n))
 	return buf.String()
 }
 
+// Text returns the text joined by all the direct child text nodes of this node.
 func (n *Node) Text() (text string) {
 	for _, c := range n.ChildrenNodes() {
-		if c.Type != html.TextNode {
+		if !c.IsTextNode() {
 			continue
 		}
 		if blankRegexp.MatchString(c.Data) {
@@ -137,10 +176,11 @@ func (n *Node) Text() (text string) {
 	return
 }
 
+// Text returns the text joined by all the child text nodes in depth order of this node.
 func (n *Node) FullText() string {
 	var buf bytes.Buffer
 	n.Walk(func(node *Node) error {
-		if node.Type == html.TextNode {
+		if node.IsTextNode() {
 			buf.WriteString(node.Data)
 		}
 		return nil
@@ -148,10 +188,14 @@ func (n *Node) FullText() string {
 	return buf.String()
 }
 
+// Walk walks the node tree rooted at this node, calling fn for each node in the tree, including this node.
 func (n *Node) Walk(fn WalkFunc) error {
 	return Walk(n, fn)
 }
 
+// Find returns the first child element node matched by the specified tag and optional attribute key and value of this node.
+//
+// It returns an error if no child element node is matched.
 func (n *Node) Find(tag string, attrkv ...string) (*Node, error) {
 	if n == nil {
 		return nil, fmt.Errorf("not allow to find on a blank node")
@@ -159,10 +203,15 @@ func (n *Node) Find(tag string, attrkv ...string) (*Node, error) {
 	if ns := query(n, tag, attrkv, 1); len(ns) > 0 {
 		return ns[0], nil
 	} else {
-		return nil, fmt.Errorf("not found element %s", prettyTagAttr(tag, attrkv))
+		return nil, fmt.Errorf("not found element `%s`", prettyTagAttr(tag, attrkv))
 	}
 }
 
+// Query returns the first child element node matched by the specified tag and optional attribute key and value of this node.
+//
+// It returns nil if no child element node is matched.
+//
+// Allow chaining call.
 func (n *Node) Query(tag string, attrkv ...string) *Node {
 	if n == nil {
 		return nil
@@ -174,6 +223,11 @@ func (n *Node) Query(tag string, attrkv ...string) *Node {
 	}
 }
 
+// QueryAll returns the child element nodes matched by the specified tag and optional attribute key and value of this node.
+//
+// It returns nil if no child element node is matched.
+//
+// Allow chaining call.
 func (n *Node) QueryAll(tag string, attrkv ...string) Nodes {
 	if n == nil {
 		return nil
@@ -183,6 +237,11 @@ func (n *Node) QueryAll(tag string, attrkv ...string) Nodes {
 
 type Nodes []*Node
 
+// Query returns all the first child element node matched by the specified tag and optional attribute key and value on each node of this nodes.
+//
+// It returns nil if no child element node is matched.
+//
+// Allow chaining call.
 func (ns Nodes) Query(tag string, attrkv ...string) (found Nodes) {
 	if ns == nil {
 		return
@@ -195,6 +254,11 @@ func (ns Nodes) Query(tag string, attrkv ...string) (found Nodes) {
 	return
 }
 
+// QueryAll returns all the child element nodes matched by the specified tag and optional attribute key and value on each node of this nodes.
+//
+// It returns nil if no child element node is matched.
+//
+// Allow chaining call.
 func (ns Nodes) QueryAll(tag string, attrkv ...string) (found Nodes) {
 	if ns == nil {
 		return
@@ -220,9 +284,9 @@ func plainAttr(attrkv []string) (string, string) {
 func prettyTagAttr(tag string, attrkv []string) string {
 	key, val := plainAttr(attrkv)
 	if key == "" && val == "" {
-		return fmt.Sprintf("`%s`", tag)
+		return tag
 	} else {
-		return fmt.Sprintf("`%s` with attribute `%s=%s`", tag, key, val)
+		return fmt.Sprintf("%s[%s=%s]", tag, key, val)
 	}
 }
 
@@ -243,7 +307,7 @@ func query(n *Node, tag string, attrkv []string, m int) (found Nodes) {
 }
 
 func match(n *Node, tag string, attrkv []string) bool {
-	if n.Type != html.ElementNode {
+	if !n.IsElementNode() {
 		return false
 	}
 	if tag != "" && tag != n.Data {
--- node_test.go
+++ node_test.go
@@ -1,6 +1,7 @@
 package supersimplesoup
 
 import (
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -12,21 +13,24 @@ const testHTML = `
 	</head>
 	<body>
 		<div>
-			<ul>
-				<li>
-					<a id="1st id" href="1st href" title="1st title" class="link">1st text</a>
+			<ul id="ul-id-1" title="ul-title-1" class="ul-class-1">
+				<li id="li-id-1" title="li-title-1" class="li-class-1">
+					<a id="a-id-1" href="a-href-1" title="a-title-1" class="a-class-1">a-text-1</a>
+					<a id="a-id-2" href="a-href-2" title="a-title-2" class="a-class-1">a-text-2</a>
 				</li>
-				<li>
-					<a id="2nd id" href="2nd href" title="2nd title" class="link">2nd text</a>
+				<li id="li-id-2" title="li-title-2" class="li-class-1">
+					<a id="a-id-3" href="a-href-3" title="a-title-3" class="a-class-1">a-text-3</a>
+					<a id="a-id-4" href="a-href-4" title="a-title-4" class="a-class-1">a-text-4</a>
 				</li>
-				<li>
-					<a id="3rd id" href="3rd href" title="3rd title" class="link">3rd text</a>
+			</ul>
+			<ul id="ul-id-2" title="ul-title-2" class="ul-class-2">
+				<li id="li-id-3" title="li-title-3" class="li-class-2">
+					<a id="a-id-5" href="a-href-5" title="a-title-5" class="a-class-2">a-text-5</a>
+					<a id="a-id-6" href="a-href-6" title="a-title-6" class="a-class-2">a-text-6</a>
 				</li>
-				<li>
-					<a id="4th id" href="4th href" title="4th title" class="link">4th text</a>
-				</li>
-				<li>
-					<a id="5th id" href="5th href" title="5th title" class="link">5th text</a>
+				<li id="li-id-4" title="li-title-4" class="li-class-2">
+					<a id="a-id-7" href="a-href-7" title="a-title-7" class="a-class-2">a-text-7</a>
+					<a id="a-id-8" href="a-href-8" title="a-title-8" class="a-class-2">a-text-8</a>
 				</li>
 			</ul>
 		</div>
@@ -34,80 +38,419 @@ const testHTML = `
 </html>
 `
 
-func TestWalk(t *testing.T) {
+func getTestNode() *Node {
 	n, err := Parse(strings.NewReader(testHTML))
 	if err != nil {
-		t.Fatal(err)
+		panic(err)
 	}
-	err = Walk(n, func(node *Node) error {
-		t.Logf("%d - %q", node.Type, node.Data)
+	return n
+}
+
+var root *Node = getTestNode()
+
+func TestWalk(t *testing.T) {
+	want := 19
+	got := 0
+	Walk(root, func(node *Node) error {
+		if node.IsElementNode() {
+			got++
+		}
 		return nil
 	})
-	if err != nil {
-		t.Error(err)
+	if want != got {
+		t.Errorf("element node count, want %d, got %d", want, got)
 	}
 }
 
 func TestFind(t *testing.T) {
-	type expected struct {
-		id       string
-		class    string
-		title    string
-		href     string
-		text     string
-		fulltext string
-	}
-	type testcase struct {
-		tag      string
-		attrkv   []string
-		expected expected
+	tests := []struct {
+		tag    string
+		attrkv []string
+	}{
+		{"title", nil},
+		{"div", nil},
+		{"ul", nil},
+		{"ul", []string{"id"}},
+		{"ul", []string{"id", "ul-id-1"}},
+		{"ul", []string{"id", "ul-id-2"}},
+		{"li", nil},
+		{"li", []string{"id"}},
+		{"li", []string{"id", "li-id-1"}},
+		{"li", []string{"id", "li-id-2"}},
+		{"li", []string{"id", "li-id-3"}},
+		{"li", []string{"id", "li-id-4"}},
+		{"a", nil},
+		{"a", []string{"id"}},
+		{"a", []string{"id", "a-id-1"}},
+		{"a", []string{"id", "a-id-2"}},
+		{"a", []string{"id", "a-id-3"}},
+		{"a", []string{"id", "a-id-4"}},
+		{"a", []string{"id", "a-id-5"}},
+		{"a", []string{"id", "a-id-6"}},
+		{"a", []string{"id", "a-id-7"}},
+		{"a", []string{"id", "a-id-8"}},
+		{"a", []string{"title", "a-title-1"}},
+		{"a", []string{"title", "a-title-2"}},
+		{"a", []string{"title", "a-title-3"}},
+		{"a", []string{"title", "a-title-4"}},
+		{"a", []string{"title", "a-title-5"}},
+		{"a", []string{"title", "a-title-6"}},
+		{"a", []string{"title", "a-title-7"}},
+		{"a", []string{"title", "a-title-8"}},
+		{"a", []string{"class", "a-class-1"}},
+		{"a", []string{"class", "a-class-2"}},
 	}
 
-	n, err := Parse(strings.NewReader(testHTML))
-	if err != nil {
-		t.Fatal(err)
+	want := 1
+	for _, test := range tests {
+		got := 0
+		node, err := root.Find(test.tag, test.attrkv...)
+		if err == nil && node != nil {
+			got = 1
+		}
+		if want != got {
+			t.Errorf("`%s` element node count, want %d, got %d", prettyTagAttr(test.tag, test.attrkv), want, got)
+		}
+	}
+}
+
+func TestQuery(t *testing.T) {
+	tests := []struct {
+		tag    string
+		attrkv []string
+	}{
+		{"title", nil},
+		{"div", nil},
+		{"ul", nil},
+		{"ul", []string{"id"}},
+		{"ul", []string{"id", "ul-id-1"}},
+		{"ul", []string{"id", "ul-id-2"}},
+		{"li", nil},
+		{"li", []string{"id"}},
+		{"li", []string{"id", "li-id-1"}},
+		{"li", []string{"id", "li-id-2"}},
+		{"li", []string{"id", "li-id-3"}},
+		{"li", []string{"id", "li-id-4"}},
+		{"a", nil},
+		{"a", []string{"id"}},
+		{"a", []string{"id", "a-id-1"}},
+		{"a", []string{"id", "a-id-2"}},
+		{"a", []string{"id", "a-id-3"}},
+		{"a", []string{"id", "a-id-4"}},
+		{"a", []string{"id", "a-id-5"}},
+		{"a", []string{"id", "a-id-6"}},
+		{"a", []string{"id", "a-id-7"}},
+		{"a", []string{"id", "a-id-8"}},
+		{"a", []string{"title", "a-title-1"}},
+		{"a", []string{"title", "a-title-2"}},
+		{"a", []string{"title", "a-title-3"}},
+		{"a", []string{"title", "a-title-4"}},
+		{"a", []string{"title", "a-title-5"}},
+		{"a", []string{"title", "a-title-6"}},
+		{"a", []string{"title", "a-title-7"}},
+		{"a", []string{"title", "a-title-8"}},
+		{"a", []string{"class", "a-class-1"}},
+		{"a", []string{"class", "a-class-2"}},
 	}
 
-	testcases := []testcase{
-		{"title", nil, expected{"", "", "", "", "supersimplesoup", "supersimplesoup"}},
-		{"a", nil, expected{"1st id", "link", "1st title", "1st href", "1st text", "1st text"}},
-		{"a", []string{"id"}, expected{"1st id", "link", "1st title", "1st href", "1st text", "1st text"}},
-		{"a", []string{"id", "1st id"}, expected{"1st id", "link", "1st title", "1st href", "1st text", "1st text"}},
-		{"a", []string{"id", "2nd id"}, expected{"2nd id", "link", "2nd title", "2nd href", "2nd text", "2nd text"}},
-		{"a", []string{"id", "3rd id"}, expected{"3rd id", "link", "3rd title", "3rd href", "3rd text", "3rd text"}},
-		{"a", []string{"id", "4th id"}, expected{"4th id", "link", "4th title", "4th href", "4th text", "4th text"}},
-		{"a", []string{"id", "5th id"}, expected{"5th id", "link", "5th title", "5th href", "5th text", "5th text"}},
+	want := 1
+	for _, test := range tests {
+		got := 0
+		node := root.Query(test.tag, test.attrkv...)
+		if node != nil {
+			got = 1
+		}
+		if want != got {
+			t.Errorf("`%s` element node count, want %d, got %d", prettyTagAttr(test.tag, test.attrkv), want, got)
+		}
+	}
+}
+
+func TestQueryAll(t *testing.T) {
+	tests := []struct {
+		tag    string
+		attrkv []string
+		want   int
+	}{
+		{"title", nil, 1},
+		{"div", nil, 1},
+		{"ul", nil, 2},
+		{"ul", []string{"id"}, 2},
+		{"ul", []string{"id", "ul-id-1"}, 1},
+		{"ul", []string{"id", "ul-id-2"}, 1},
+		{"li", nil, 4},
+		{"li", []string{"id"}, 4},
+		{"li", []string{"id", "li-id-1"}, 1},
+		{"li", []string{"id", "li-id-2"}, 1},
+		{"li", []string{"id", "li-id-3"}, 1},
+		{"li", []string{"id", "li-id-4"}, 1},
+		{"a", nil, 8},
+		{"a", []string{"id"}, 8},
+		{"a", []string{"id", "a-id-1"}, 1},
+		{"a", []string{"id", "a-id-2"}, 1},
+		{"a", []string{"id", "a-id-3"}, 1},
+		{"a", []string{"id", "a-id-4"}, 1},
+		{"a", []string{"id", "a-id-5"}, 1},
+		{"a", []string{"id", "a-id-6"}, 1},
+		{"a", []string{"id", "a-id-7"}, 1},
+		{"a", []string{"id", "a-id-8"}, 1},
+		{"a", []string{"title", "a-title-1"}, 1},
+		{"a", []string{"title", "a-title-2"}, 1},
+		{"a", []string{"title", "a-title-3"}, 1},
+		{"a", []string{"title", "a-title-4"}, 1},
+		{"a", []string{"title", "a-title-5"}, 1},
+		{"a", []string{"title", "a-title-6"}, 1},
+		{"a", []string{"title", "a-title-7"}, 1},
+		{"a", []string{"title", "a-title-8"}, 1},
+		{"a", []string{"class", "a-class-1"}, 4},
+		{"a", []string{"class", "a-class-2"}, 4},
 	}
 
-	for _, testcase := range testcases {
-		found, err := n.Find(testcase.tag, testcase.attrkv...)
-		if err != nil {
-			t.Errorf("%s => unexpected error %q", testcase.tag, err)
-		} else {
-			actual := found.ID()
-			if actual != testcase.expected.id {
-				t.Errorf("%s => expected %q, got %q", testcase.tag, testcase.expected.id, actual)
-			}
-			actual = found.Class()
-			if actual != testcase.expected.class {
-				t.Errorf("%s => expected %q, got %q", testcase.tag, testcase.expected.class, actual)
-			}
-			actual = found.Title()
-			if actual != testcase.expected.title {
-				t.Errorf("%s => expected %q, got %q", testcase.tag, testcase.expected.title, actual)
-			}
-			actual = found.Href()
-			if actual != testcase.expected.href {
-				t.Errorf("%s => expected %q, got %q", testcase.tag, testcase.expected.href, actual)
-			}
-			actual = found.Text()
-			if actual != testcase.expected.text {
-				t.Errorf("%s => expected %q, got %q", testcase.tag, testcase.expected.text, actual)
-			}
-			actual = found.FullText()
-			if actual != testcase.expected.fulltext {
-				t.Errorf("%s => expected %q, got %q", testcase.tag, testcase.expected.fulltext, actual)
-			}
+	for _, test := range tests {
+		nodes := root.QueryAll(test.tag, test.attrkv...)
+		got := len(nodes)
+		if test.want != got {
+			t.Errorf("`%s` element node count, want %d, got %d", prettyTagAttr(test.tag, test.attrkv), test.want, got)
+		}
+	}
+}
+
+func TestQueryChain(t *testing.T) {
+	want := []string{"a-id-1"}
+	var got []string
+	if node := root.Query("ul").Query("li").Query("a"); node != nil {
+		got = append(got, node.ID())
+	}
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("`%s` element attribute id, want %v, got %v", "ul.li.a", want, got)
+	}
+
+	want = []string{"a-id-1", "a-id-2"}
+	got = nil
+	for _, node := range root.Query("ul").Query("li").QueryAll("a") {
+		got = append(got, node.ID())
+	}
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("`%s` element attribute id, want %v, got %v", "ul.li.$$a", want, got)
+	}
+
+	want = []string{"a-id-1", "a-id-3"}
+	got = nil
+	for _, node := range root.Query("ul").QueryAll("li").Query("a") {
+		got = append(got, node.ID())
+	}
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("`%s` element attribute id, want %v, got %v", "ul.$$li.a", want, got)
+	}
+
+	want = []string{"a-id-1", "a-id-2", "a-id-3", "a-id-4"}
+	got = nil
+	for _, node := range root.Query("ul").QueryAll("li").QueryAll("a") {
+		got = append(got, node.ID())
+	}
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("`%s` element attribute id, want %v, got %v", "ul.$$li.$$a", want, got)
+	}
+
+	want = []string{"a-id-1", "a-id-5"}
+	got = nil
+	for _, node := range root.QueryAll("ul").Query("li").Query("a") {
+		got = append(got, node.ID())
+	}
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("`%s` element attribute id, want %v, got %v", "$$ul.li.a", want, got)
+	}
+
+	want = []string{"a-id-1", "a-id-2", "a-id-5", "a-id-6"}
+	got = nil
+	for _, node := range root.QueryAll("ul").Query("li").QueryAll("a") {
+		got = append(got, node.ID())
+	}
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("`%s` element attribute id, want %v, got %v", "$$ul.li.$$a", want, got)
+	}
+
+	want = []string{"a-id-1", "a-id-3", "a-id-5", "a-id-7"}
+	got = nil
+	for _, node := range root.QueryAll("ul").QueryAll("li").Query("a") {
+		got = append(got, node.ID())
+	}
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("`%s` element attribute id, want %v, got %v", "$$ul.$$li.a", want, got)
+	}
+
+	want = []string{"a-id-1", "a-id-2", "a-id-3", "a-id-4", "a-id-5", "a-id-6", "a-id-7", "a-id-8"}
+	got = nil
+	for _, node := range root.QueryAll("ul").QueryAll("li").QueryAll("a") {
+		got = append(got, node.ID())
+	}
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("`%s` element attribute id, want %v, got %v", "$$ul.$$li.$$a", want, got)
+	}
+}
+
+func TestText(t *testing.T) {
+	tests := []struct {
+		tag    string
+		attrkv []string
+		want   string
+	}{
+		{"title", nil, "supersimplesoup"},
+		{"div", nil, ""},
+		{"ul", nil, ""},
+		{"ul", []string{"id"}, ""},
+		{"ul", []string{"id", "ul-id-1"}, ""},
+		{"ul", []string{"id", "ul-id-2"}, ""},
+		{"li", nil, ""},
+		{"li", []string{"id"}, ""},
+		{"li", []string{"id", "li-id-1"}, ""},
+		{"li", []string{"id", "li-id-2"}, ""},
+		{"li", []string{"id", "li-id-3"}, ""},
+		{"li", []string{"id", "li-id-4"}, ""},
+		{"a", nil, "a-text-1"},
+		{"a", []string{"id"}, "a-text-1"},
+		{"a", []string{"id", "a-id-1"}, "a-text-1"},
+		{"a", []string{"id", "a-id-2"}, "a-text-2"},
+		{"a", []string{"id", "a-id-3"}, "a-text-3"},
+		{"a", []string{"id", "a-id-4"}, "a-text-4"},
+		{"a", []string{"id", "a-id-5"}, "a-text-5"},
+		{"a", []string{"id", "a-id-6"}, "a-text-6"},
+		{"a", []string{"id", "a-id-7"}, "a-text-7"},
+		{"a", []string{"id", "a-id-8"}, "a-text-8"},
+		{"a", []string{"title", "a-title-1"}, "a-text-1"},
+		{"a", []string{"title", "a-title-2"}, "a-text-2"},
+		{"a", []string{"title", "a-title-3"}, "a-text-3"},
+		{"a", []string{"title", "a-title-4"}, "a-text-4"},
+		{"a", []string{"title", "a-title-5"}, "a-text-5"},
+		{"a", []string{"title", "a-title-6"}, "a-text-6"},
+		{"a", []string{"title", "a-title-7"}, "a-text-7"},
+		{"a", []string{"title", "a-title-8"}, "a-text-8"},
+		{"a", []string{"class", "a-class-1"}, "a-text-1"},
+		{"a", []string{"class", "a-class-2"}, "a-text-5"},
+	}
+
+	for _, test := range tests {
+		node := root.Query(test.tag, test.attrkv...)
+		if node == nil {
+			t.Errorf("`%s` element node count, want %d, got %d", prettyTagAttr(test.tag, test.attrkv), 1, 0)
+			continue
+		}
+		got := node.Text()
+		if test.want != got {
+			t.Errorf("`%s` element node text, want %q, got %q", prettyTagAttr(test.tag, test.attrkv), test.want, got)
+		}
+	}
+}
+
+func TestFullText(t *testing.T) {
+	tests := []struct {
+		tag    string
+		attrkv []string
+		want   string
+	}{
+		{"title", nil, "supersimplesoup"},
+		{"li", nil, "\n\t\t\t\t\ta-text-1\n\t\t\t\t\ta-text-2\n\t\t\t\t"},
+		{"li", []string{"id"}, "\n\t\t\t\t\ta-text-1\n\t\t\t\t\ta-text-2\n\t\t\t\t"},
+		{"li", []string{"id", "li-id-1"}, "\n\t\t\t\t\ta-text-1\n\t\t\t\t\ta-text-2\n\t\t\t\t"},
+		{"li", []string{"id", "li-id-2"}, "\n\t\t\t\t\ta-text-3\n\t\t\t\t\ta-text-4\n\t\t\t\t"},
+		{"li", []string{"id", "li-id-3"}, "\n\t\t\t\t\ta-text-5\n\t\t\t\t\ta-text-6\n\t\t\t\t"},
+		{"li", []string{"id", "li-id-4"}, "\n\t\t\t\t\ta-text-7\n\t\t\t\t\ta-text-8\n\t\t\t\t"},
+		{"a", nil, "a-text-1"},
+		{"a", []string{"id"}, "a-text-1"},
+		{"a", []string{"id", "a-id-1"}, "a-text-1"},
+		{"a", []string{"id", "a-id-2"}, "a-text-2"},
+		{"a", []string{"id", "a-id-3"}, "a-text-3"},
+		{"a", []string{"id", "a-id-4"}, "a-text-4"},
+		{"a", []string{"id", "a-id-5"}, "a-text-5"},
+		{"a", []string{"id", "a-id-6"}, "a-text-6"},
+		{"a", []string{"id", "a-id-7"}, "a-text-7"},
+		{"a", []string{"id", "a-id-8"}, "a-text-8"},
+		{"a", []string{"title", "a-title-1"}, "a-text-1"},
+		{"a", []string{"title", "a-title-2"}, "a-text-2"},
+		{"a", []string{"title", "a-title-3"}, "a-text-3"},
+		{"a", []string{"title", "a-title-4"}, "a-text-4"},
+		{"a", []string{"title", "a-title-5"}, "a-text-5"},
+		{"a", []string{"title", "a-title-6"}, "a-text-6"},
+		{"a", []string{"title", "a-title-7"}, "a-text-7"},
+		{"a", []string{"title", "a-title-8"}, "a-text-8"},
+		{"a", []string{"class", "a-class-1"}, "a-text-1"},
+		{"a", []string{"class", "a-class-2"}, "a-text-5"},
+	}
+
+	for _, test := range tests {
+		node := root.Query(test.tag, test.attrkv...)
+		if node == nil {
+			t.Errorf("`%s` element node count, want %d, got %d", prettyTagAttr(test.tag, test.attrkv), 1, 0)
+			continue
+		}
+		got := node.FullText()
+		if test.want != got {
+			t.Errorf("`%s` element node full text, want %q, got %q", prettyTagAttr(test.tag, test.attrkv), test.want, got)
+		}
+	}
+}
+
+func TestAttribute(t *testing.T) {
+	tests := []struct {
+		tag    string
+		attrkv []string
+		want   []string
+	}{
+		{"title", nil, []string{"", "", "", ""}},
+		{"div", nil, []string{"", "", "", ""}},
+		{"ul", nil, []string{"ul-id-1", "ul-class-1", "ul-title-1", ""}},
+		{"ul", []string{"id"}, []string{"ul-id-1", "ul-class-1", "ul-title-1", ""}},
+		{"ul", []string{"id", "ul-id-1"}, []string{"ul-id-1", "ul-class-1", "ul-title-1", ""}},
+		{"ul", []string{"id", "ul-id-2"}, []string{"ul-id-2", "ul-class-2", "ul-title-2", ""}},
+		{"li", nil, []string{"li-id-1", "li-class-1", "li-title-1", ""}},
+		{"li", []string{"id"}, []string{"li-id-1", "li-class-1", "li-title-1", ""}},
+		{"li", []string{"id", "li-id-1"}, []string{"li-id-1", "li-class-1", "li-title-1", ""}},
+		{"li", []string{"id", "li-id-2"}, []string{"li-id-2", "li-class-1", "li-title-2", ""}},
+		{"li", []string{"id", "li-id-3"}, []string{"li-id-3", "li-class-2", "li-title-3", ""}},
+		{"li", []string{"id", "li-id-4"}, []string{"li-id-4", "li-class-2", "li-title-4", ""}},
+		{"a", nil, []string{"a-id-1", "a-class-1", "a-title-1", "a-href-1"}},
+		{"a", []string{"id"}, []string{"a-id-1", "a-class-1", "a-title-1", "a-href-1"}},
+		{"a", []string{"id", "a-id-1"}, []string{"a-id-1", "a-class-1", "a-title-1", "a-href-1"}},
+		{"a", []string{"id", "a-id-2"}, []string{"a-id-2", "a-class-1", "a-title-2", "a-href-2"}},
+		{"a", []string{"id", "a-id-3"}, []string{"a-id-3", "a-class-1", "a-title-3", "a-href-3"}},
+		{"a", []string{"id", "a-id-4"}, []string{"a-id-4", "a-class-1", "a-title-4", "a-href-4"}},
+		{"a", []string{"id", "a-id-5"}, []string{"a-id-5", "a-class-2", "a-title-5", "a-href-5"}},
+		{"a", []string{"id", "a-id-6"}, []string{"a-id-6", "a-class-2", "a-title-6", "a-href-6"}},
+		{"a", []string{"id", "a-id-7"}, []string{"a-id-7", "a-class-2", "a-title-7", "a-href-7"}},
+		{"a", []string{"id", "a-id-8"}, []string{"a-id-8", "a-class-2", "a-title-8", "a-href-8"}},
+		{"a", []string{"title", "a-title-1"}, []string{"a-id-1", "a-class-1", "a-title-1", "a-href-1"}},
+		{"a", []string{"title", "a-title-2"}, []string{"a-id-2", "a-class-1", "a-title-2", "a-href-2"}},
+		{"a", []string{"title", "a-title-3"}, []string{"a-id-3", "a-class-1", "a-title-3", "a-href-3"}},
+		{"a", []string{"title", "a-title-4"}, []string{"a-id-4", "a-class-1", "a-title-4", "a-href-4"}},
+		{"a", []string{"title", "a-title-5"}, []string{"a-id-5", "a-class-2", "a-title-5", "a-href-5"}},
+		{"a", []string{"title", "a-title-6"}, []string{"a-id-6", "a-class-2", "a-title-6", "a-href-6"}},
+		{"a", []string{"title", "a-title-7"}, []string{"a-id-7", "a-class-2", "a-title-7", "a-href-7"}},
+		{"a", []string{"title", "a-title-8"}, []string{"a-id-8", "a-class-2", "a-title-8", "a-href-8"}},
+		{"a", []string{"class", "a-class-1"}, []string{"a-id-1", "a-class-1", "a-title-1", "a-href-1"}},
+		{"a", []string{"class", "a-class-2"}, []string{"a-id-5", "a-class-2", "a-title-5", "a-href-5"}},
+	}
+
+	for _, test := range tests {
+		node := root.Query(test.tag, test.attrkv...)
+		if node == nil {
+			t.Errorf("`%s` element node count, want %d, got %d", prettyTagAttr(test.tag, test.attrkv), 1, 0)
+			continue
+		}
+		got := node.ID()
+		if test.want[0] != got {
+			t.Errorf("`%s` element node attribute id, want %q, got %q", prettyTagAttr(test.tag, test.attrkv), test.want[0], got)
+		}
+		got = node.Class()
+		if test.want[1] != got {
+			t.Errorf("`%s` element node attribute class, want %q, got %q", prettyTagAttr(test.tag, test.attrkv), test.want[1], got)
+		}
+		got = node.Title()
+		if test.want[2] != got {
+			t.Errorf("`%s` element node attribute title, want %q, got %q", prettyTagAttr(test.tag, test.attrkv), test.want[2], got)
+		}
+		got = node.Href()
+		if test.want[3] != got {
+			t.Errorf("`%s` element node attribute href, want %q, got %q", prettyTagAttr(test.tag, test.attrkv), test.want[3], got)
 		}
 	}
 }
